870cc7954e75f777aaebb5286bfdfa339ef5753a
Allison Bradley <allison.bradley@outlook.com>
2019-12-16 19:34:04 -0700

    added question functionality for first question

---
+++ assets/questions.js
@@ -0,0 +1,13 @@
+var questions = [
+    {
+        title: "Barf:",
+        choices: ["Now", "Right now", "How about now", "for sure"],
+        answer: "Now"
+    },
+    {
+        title: "Second question:",
+        choices: ["first", "second", "third", "fourth"],
+        answer: "third"
+    },
+
+];
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 var startBtn = document.getElementById("startButton")
 var counter = document.getElementById("timeRemaining")
 var landingPage = document.getElementById("landingPage")
-var page2 = document.getElementById("page2")
+var question = document.getElementById("question")
+var optionOne = document.getElementById("optionOne")
+var optionTwo = document.getElementById("optionTwo")
+var optionThree = document.getElementById("optionThree")
+var optionFour = document.getElementById("optionFour")
+var answerEl = document.getElementById("answer")
 
 var timeRemaining = 60;
+
 
 counter.textContent = (`Time Remaining: ${timeRemaining} seconds`)
 
@@ -11,7 +17,7 @@ function startCountdown() {
     var timerInterval = setInterval(function () {
         timeRemaining--;
         counter.textContent = (`Time Remaining: ${timeRemaining} seconds`);
-
+        
         if (timeRemaining === -1) {
             clearInterval(timerInterval);
             alert("Time's up")
@@ -21,15 +27,55 @@ function startCountdown() {
     }, 100);
 }
 
+
 startBtn.addEventListener("click", function (event) {
     event.preventDefault();
     event.stopPropagation();
     landingPage.setAttribute("class", "hidden")
-    page2.setAttribute("class", "show")
+    question.textContent = (questions[0].title)
+    optionOne.textContent = (questions[0].choices[0])
+    optionTwo.textContent = (questions[0].choices[1])
+    optionThree.textContent = (questions[0].choices[2])
+    optionFour.textContent = (questions[0].choices[3])
+    var answer = (questions[0].answer[0])
+    questionPage.setAttribute("class", "show")
     startCountdown();
-
+    
 });
 
+optionOne.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var userChoice = (questions[0].choices[0])
+    
+    if (userChoice.String == answer.String) {
+        answerEl.textContent=("Correct!")
+    } else {answerEl.textContent=("Incorrect")}
+
+    question.textContent = (questions[1].title)
+    optionOne.textContent = (questions[1].choices[0])
+    optionTwo.textContent = (questions[1].choices[1])
+    optionThree.textContent = (questions[1].choices[2])
+    optionFour.textContent = (questions[1].choices[3])
+
+})
+
+optionOne.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var userChoice = (questions[0].choices[0])
+    
+    if (userChoice.String == answer.String) {
+        answerEl.textContent=("Correct!")
+    } else {answerEl.textContent=("Incorrect")}
+
+    question.textContent = (questions[1].title)
+    optionOne.textContent = (questions[1].choices[0])
+    optionTwo.textContent = (questions[1].choices[1])
+    optionThree.textContent = (questions[1].choices[2])
+    optionFour.textContent = (questions[1].choices[3])
+
+})
 
 
 
